facec1eaf9e007f18ab749dba6fc32ed20899e3a
shizukani-cp <to.maimai0606@gmail.com>
2024-06-22 11:38:19 +0900

    引数の解析用モジュールをargparseに変更・本体部分を実装

--- src/mecame/__init__.py
+++ src/mecame/__init__.py
@@ -1,2 +1,19 @@
-import sys
+import argparse
 from janome.tokenizer import Tokenizer
+
+parser = argparse.ArgumentParser(description="This is mecab(?).")
+parser.add_argument("fromf")
+parser.add_argument("savef")
+args = parser.parse_args()
+FROMF = args.fromf
+SAVEF = args.savef
+tokenizer = Tokenizer()
+
+text = ""
+with open(FROMF, "r", encoding="utf-8") as f:
+    for row in f:
+        for t in tokenizer.tokenize(row):
+            text += f"{str(t)}\n"
+
+with open(SAVEF, "w", encoding="utf-8") as f:
+    f.write(text)

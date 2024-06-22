c7a57753b9cba72f730b13974e8c7461df4e3c7b
shizukani-cp <to.maimai0606@gmail.com>
2024-06-22 11:48:30 +0900

    文が一行に続いていたため修正

--- mecame.py
+++ mecame.py
@@ -12,8 +12,10 @@ tokenizer = Tokenizer()
 text = ""
 with open(FROMF, "r", encoding="utf-8") as f:
     for row in f:
+        row_t = ""
         for t in tokenizer.tokenize(row, wakati=True):
-            text += f"{str(t)} "
+            row_t += f"{str(t)} "
+        text += f"{row_t}\n"
 
 with open(SAVEF, "w", encoding="utf-8") as f:
     f.write(text)
